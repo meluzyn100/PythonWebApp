ac02bbfda27a8ed9cb7bb3d6c5428a23b7ab24c1
meluzyn100 <255939@student.pwr.edu.pl>
2022-10-21 00:18:26 +0200

    test

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a sample Python Web App running on Flask Framework!"
+    return "Hello, this is a sample Python Web App running on Flask Framework!!!"
+
+@app.route("/name")
+def name():
+    return "Hello name"
 
